03935440958c00005e6011834c91aa42fb50d7c7
rosendito <rosendojose0617@gmail.com>
2021-09-29 09:52:45 -0400

    feat(database): Add migrations for terms, taxonomies and taggables tables

---
+++ database/migrations/2021_06_09_210421_create_terms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTermsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('terms', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('taxonomy_id')->constrained('taxonomies');
+            $table->foreignId('parent_id')->nullable()->constrained('terms');
+            $table->string('name');
+            $table->integer('hierarchy')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('terms');
+    }
+}
