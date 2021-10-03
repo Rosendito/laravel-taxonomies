aceab90c9fd0995657b3c942f63bfd35fb0d07e4
rosendito <rosendojose0617@gmail.com>
2021-10-03 12:16:05 -0400

    test: Create tests for Term model

--- tests/TermTest.php
+++ tests/TermTest.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Rosendito\Taxonomies\Taxonomy;
+use Rosendito\Taxonomies\Term;
 use Tests\Support\Post;
 
 class TermTest extends TestCase
@@ -18,18 +19,86 @@ class TermTest extends TestCase
     ];
 
     /**
-     * Test create taxonomies
+     * SetUp test environment
      *
      * @return void
      */
-    public function test_create_taxonomies(): void
+    public function setUp(): void
     {
-        foreach ($this->taxonomies as $taxonomy) {
+        parent::setUp();
+
+        foreach ($this->taxonomies as $name) {
             Taxonomy::create([
-                'name' => $taxonomy
+                'name' => $name
             ]);
         }
+    }
 
-        $this->assertCount(2, Taxonomy::all());
+    /**
+     * Test create terms
+     *
+     * @return void
+     */
+    public function test_create_terms(): void
+    {
+        $taxonomy = Taxonomy::where('name', $this->taxonomies[0])->first();
+
+        $term1 = Term::create([
+            'taxonomy_id' => $taxonomy->id,
+            'name' => 'Foo'
+        ]);
+
+        $term2 = Term::create([
+            'taxonomy_id' => $taxonomy->id,
+            'parent_id' => $term1->id,
+            'name' => 'Bar'
+        ]);
+
+        // Check if the two terms are created
+        $this->assertCount(2, Term::all());
+        // Check if the first term has the correct taxonomy
+        $this->assertEquals($taxonomy->id, $term1->taxonomy->id);
+        // Check if the second term has term1 as parent
+        $this->assertEquals($term2->parent->id, $term1->id);
+        // Check if the first term has term2 as child
+        $this->assertTrue($term1->childs->contains($term2));
+    }
+
+    /**
+     * Test terms scopes
+     *
+     * @return void
+     */
+    public function test_terms_scopes(): void
+    {
+        $taxonomy1 = Taxonomy::first();
+        $taxonomy2 = Taxonomy::latest()->first();
+
+        $term1 = Term::create([
+            'taxonomy_id' => $taxonomy1->id,
+            'name' => 'Foo'
+        ]);
+
+        $term2 = Term::create([
+            'taxonomy_id' => $taxonomy2->id,
+            'name' => 'Bar'
+        ]);
+
+        $term3 = Term::create([
+            'taxonomy_id' => $taxonomy1->id,
+            'parent_id' => $term2->id,
+            'name' => 'Baz'
+        ]);
+
+        // Check if scope "byTaxonomy" works correctly
+        $this->assertEquals(
+            $term1->id,
+            Term::byTaxonomy($taxonomy1->id)->first()->id
+        );
+        // Check if scope "byParent" works correctly
+        $this->assertEquals(
+            $term3->id,
+            Term::byParent($term2->name)->first()->id,
+        );
     }
 }
